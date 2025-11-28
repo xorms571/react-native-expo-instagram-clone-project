cf965cb7fe64d21caba22b3facae81ef9ce68bd3
xorms571 <xorms6865@naver.com>
2025-11-29 00:08:26 +0900

    의존성 설치 및 환경 변수 설정 superbase

---
+++ utils/supabase.ts
@@ -0,0 +1,11 @@
+import 'react-native-url-polyfill/auto';
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("Supabase URL and Anon Key are missing. Please check your .env file.");
+}
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
